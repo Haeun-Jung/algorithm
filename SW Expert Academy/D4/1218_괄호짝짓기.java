ac4db864c2b145d9e3add75b23af0c946505502f
Haeun-Jung <gkdms7070@naver.com>
2021-08-06 00:27:48 +0900

    1218 괄호 짝짓기

---
+++ SW Expert Academy/D4/1218_괄호짝짓기.java
@@ -0,0 +1,29 @@
+package com.ssafy.solution;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Solution {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		for (int tc = 1; tc <= 10; tc++) {
+			int len = sc.nextInt();								// 문자열의 길이
+			char[] ch = sc.next().toCharArray();				// 문자 배열
+			Stack<Character> stack = new Stack<Character>();	// 스택 초기화
+			
+			// ch 배열을 순회하며 stack의 마지막 괄호와 현재 ch[i]의 괄호가 유효한지 판단
+			for (int i = 0; i < len; i++) {
+				if (stack.isEmpty()) stack.push(ch[i]);
+				else if (stack.peek() == '(' && ch[i] == ')') stack.pop();
+				else if (stack.peek() == '[' && ch[i] == ']') stack.pop();
+				else if (stack.peek() == '{' && ch[i] == '}') stack.pop();
+				else if (stack.peek() == '<' && ch[i] == '>') stack.pop();
+				else stack.push(ch[i]);
+			}
+			System.out.printf("#%d %d\n", tc, stack.isEmpty() ? 1 : 0);
+		}
+		sc.close();
+	}
+}
