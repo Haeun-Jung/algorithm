defa5974291c17cfe0de781e78d0932fcd0b543f
Haeun-Jung <53832553+Haeun-Jung@users.noreply.github.com>
2021-08-26 19:05:17 +0900

    6603 로또

---
+++ Baekjoon/6603_로또.java
@@ -0,0 +1,100 @@
+/*
+독일 로또는 {1, 2, ..., 49}에서 수 6개를 고른다.
+로또 번호를 선택하는데 사용되는 가장 유명한 전략은 49가지 수 중 k(k>6)개의 수를 골라 집합 S를 만든 다음 그 수만 가지고 번호를 선택하는 것이다.
+
+예를 들어, k=8, S={1,2,3,5,8,13,21,34}인 경우 이 집합 S에서 수를 고를 수 있는 경우의 수는 총 28가지이다. 
+([1,2,3,5,8,13], [1,2,3,5,8,21], [1,2,3,5,8,34], [1,2,3,5,13,21], ..., [3,5,8,13,21,34])
+집합 S와 k가 주어졌을 때, 수를 고르는 모든 방법을 구하는 프로그램을 작성하시오.
+*/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_6603_로또 {
+	private static int[] arr, result;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		while (true) {
+			String str = br.readLine();
+			if (str.equals("0")) break;
+			String[] temp = str.split(" ");
+			// 0이면 반복문 탈출
+			
+			arr = new int[temp.length];
+			result = new int[6];
+			
+			for (int i = 1; i < temp.length; i++) {
+				arr[i] = Integer.parseInt(temp[i]);
+			}
+			// 조합
+			comb(0, 1);
+			// 빈 줄 출력
+			System.out.println();
+		}
+	}
+	
+	// 조합
+	private static void comb(int cnt, int start) {
+		// 기저조건
+		if (cnt == result.length) {
+			// 출력
+			for (int i = 0; i < result.length; i++) {
+				System.out.print(result[i] + " ");
+			}
+			System.out.println();
+			return;
+		}
+		
+		for (int i = start; i < arr.length; i++) {
+			result[cnt] = arr[i];
+			comb(cnt+1, i+1);
+		}
+	}
+}
+
+/*
+입력
+7 1 2 3 4 5 6 7
+8 1 2 3 5 8 13 21 34
+0
+
+출력
+1 2 3 4 5 6
+1 2 3 4 5 7
+1 2 3 4 6 7
+1 2 3 5 6 7
+1 2 4 5 6 7
+1 3 4 5 6 7
+2 3 4 5 6 7
+
+1 2 3 5 8 13
+1 2 3 5 8 21
+1 2 3 5 8 34
+1 2 3 5 13 21
+1 2 3 5 13 34
+1 2 3 5 21 34
+1 2 3 8 13 21
+1 2 3 8 13 34
+1 2 3 8 21 34
+1 2 3 13 21 34
+1 2 5 8 13 21
+1 2 5 8 13 34
+1 2 5 8 21 34
+1 2 5 13 21 34
+1 2 8 13 21 34
+1 3 5 8 13 21
+1 3 5 8 13 34
+1 3 5 8 21 34
+1 3 5 13 21 34
+1 3 8 13 21 34
+1 5 8 13 21 34
+2 3 5 8 13 21
+2 3 5 8 13 34
+2 3 5 8 21 34
+2 3 5 13 21 34
+2 3 8 13 21 34
+2 5 8 13 21 34
+3 5 8 13 21 34
+*/
